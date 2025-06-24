2cd506d45040b22f76f1d8007a2047ebea58408a
nichiyoo <61191577+nichiyoo@users.noreply.github.com>
2025-06-24 08:24:42 +0700

    chore: add header name to exported csv

--- app/Exports/EmployeeExport.php
+++ app/Exports/EmployeeExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Employee;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class EmployeeExport implements FromCollection
+class EmployeeExport implements FromCollection, WithHeadings
 {
   /**
    * @return \Illuminate\Support\Collection
@@ -13,5 +14,27 @@ class EmployeeExport implements FromCollection
   public function collection()
   {
     return Employee::all();
+  }
+
+  /**
+   * @return array<string>
+   */
+  public function headings(): array
+  {
+    return [
+      'id',
+      'created_at',
+      'updated_at',
+      'name',
+      'email',
+      'phone',
+      'address',
+      'birthdate',
+      'gender',
+      'religion',
+      'status',
+      'department_id',
+      'position_id',
+    ];
   }
 }
--- app/Exports/SummaryExport.php
+++ app/Exports/SummaryExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Employee;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class SummaryExport implements FromCollection
+class SummaryExport implements FromCollection, WithHeadings
 {
   /**
    * @return \Illuminate\Support\Collection
@@ -18,13 +19,18 @@ class SummaryExport implements FromCollection
     });
 
     return $employees->map(function ($employee) {
+      $employee->training_count = $employee->matrix->training_count;
+      $employee->evaluation_count = $employee->matrix->evaluation_count;
+
       $employee->feedback_score = $employee->matrix->feedback_score;
       $employee->training_score = $employee->matrix->training_score;
       $employee->evaluation_score = $employee->matrix->evaluation_score;
+
       $employee->performance_score = $employee->matrix->performance_score;
       $employee->potential_score = $employee->matrix->potential_score;
       $employee->average_score = $employee->matrix->average_score;
       $employee->segment = $employee->matrix->segment->label();
+
 
       $employee->average_score = $employee->matrix->average_score;
       $employee->average_potential = $employee->matrix->potential_score;
@@ -33,5 +39,40 @@ class SummaryExport implements FromCollection
 
       return $employee;
     });
+  }
+
+  /**
+   * @return array<string>
+   */
+  public function headings(): array
+  {
+    return [
+      'id',
+      'created_at',
+      'updated_at',
+      'name',
+      'email',
+      'phone',
+      'address',
+      'birthdate',
+      'gender',
+      'religion',
+      'status',
+      'department_id',
+      'position_id',
+      'unknown',
+      'training_count',
+      'evaluation_count',
+      'feedback_score',
+      'training_score',
+      'evaluation_score',
+      'performance_score',
+      'potential_score',
+      'average_score',
+      'segment',
+      'average_score',
+      'average_potential',
+      'average_performance',
+    ];
   }
 }
